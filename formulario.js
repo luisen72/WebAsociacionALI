edabaf1f09dd573094c1c17bc5fd90bc9eb7c413
Ivan de la torre <ivanpipi05@gmail.com>
2024-02-06 17:43:39 +0100

    Javascript formulario v0.9 y cambio de fondo añadido

--- formulario.js
+++ formulario.js
@@ -6,14 +6,35 @@ function enviarFormulario(){
             var Apellidos = document.getElementById("Apellidos").value;
             var DNI = document.getElementById("DNI").value;  
             var Gmail = document.getElementById("Gmail").value;
-            //var Estado  = document.getElementById("Estado").value;
+            var Estado  = document.getElementsByClassName("estado");
             var Teléfono = document.getElementById("Teléfono").value;
             //var Disponibilidad = document.getElementById("Disponibilidad").value;
-            //var Motivación = document.getElementById("Motivación").value;
-            //var observaciones = document.getElementById("observaciones").value;
-            //var checkbox = document.getElementById("checkbox").value;
+            var Motivación = document.getElementsByClassName("opciones");
+            var observaciones = document.getElementById("observaciones").value;
+            
+           
+          for(var i=0; i<8;i++){
+               
+               if(Motivación[i].selected){
+                   solucion = Motivación[i].value;
+               }
+            }
+            //Un array para el estado
+            var estado = new Array(3);
+            estado[0] = "Soltero"
+            estado[1] = "Casado"
+            estado[2] = "Divorciado"
+           
+           
+          for(var i=0; i<3;i++){
+               
+               if(Estado[i].checked){
+                   var resultado = estado[i];
+               }
+            }
 
-            alert("El nombre es "+Nombre+ "\n el apellido es:" +Apellidos+ "\nEl DNI es: " +DNI+ 
-            "\nEl Gmail es: "+Gmail+ "\nEl estado civil es: " + Estado+ "\nEL número de teléfono es:"
-             +Teléfono+ "La disponibilidad" +Disponibilidad+"La motivación se debe a: "+Motivación+ "La observación es : "+observación);
+
+            alert("El nombre es: "+Nombre+ "\nEl apellido es:" +Apellidos+ "\nEl DNI es: " +DNI+ 
+            "\nEl Gmail es: "+Gmail + "\nEl estado civil es: " + resultado+ "\nEL número de teléfono es:"
+             +Teléfono+ /*"La disponibilidad" +Disponibilidad+*/ "\nLa motivación se debe a: "+solucion+ "\nLa observación es : "+observaciones);
         }
